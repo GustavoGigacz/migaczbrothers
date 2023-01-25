65799f54758e4d0489a9cc642c71bba4aa662057
GustavoGigacz <gustavomigacz@gmail.com>
2023-01-25 14:36:02 -0300

    extract navbar to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import GlobalStyle from "../components/styles/globalStyles";
 import Variables from "../components/styles/variables";
 import type { AppProps } from "next/app";
+import NavBar from "../components/NavBar";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GlobalStyle />
       <Variables />
+      <NavBar />
       <Component {...pageProps} />
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import NavBar from "../components/NavBar"
-
 export default function Home() {
   return (
     <>
-      <NavBar />
+      <h1>Home Page</h1>
     </>
-  )
+  );
 }
-
-
