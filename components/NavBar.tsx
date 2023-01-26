1fae5f9b2f7cdb843fb8b06ac364cba092d1c569
GustavoGigacz <gustavomigacz@gmail.com>
2023-01-26 14:12:36 -0300

    add navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,13 +3,54 @@ import MigaczBrothersIcon from "./icons/MigaczBrother";
 
 const NavBar = (): JSX.Element => {
   return (
-    <NavBarContent>
-      <MigaczBrothersIcon />
-    </NavBarContent>
+    <NavContainer>
+      <Nav>
+        <MigaczBrothersIcon />
+        <NavList>
+          <NavLink>Início</NavLink>
+          <NavLink>Sobre nós</NavLink>
+          <NavLink>Depoimentos</NavLink>
+          <ContactButton>Contacte-nos</ContactButton>
+        </NavList>
+      </Nav>
+    </NavContainer>
   );
 };
 
-const NavBarContent = styled.div`
+const NavContainer = styled.div`
   background: var(--dark-primary);
+  width: 100%;
+  padding: 24px 0;
+`;
+
+const Nav = styled.div`
+  width: 1400px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 16px;
+`;
+
+const NavList = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const NavLink = styled.li`
+  color: var(--white-color-font);
+  font-size: 16px;
+  padding: 0 20px;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const ContactButton = styled.button`
+  width: 220px;
+  height: 42px;
+  background-color: var(--green-color);
+  color: var(--white-color-font);
+  border-radius: var(--rounded);
 `;
 export default NavBar;
