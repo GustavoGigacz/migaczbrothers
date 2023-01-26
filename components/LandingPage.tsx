c1d2ecf392c60ea12ce6677cf443c800384efc09
GustavoGigacz <gustavomigacz@gmail.com>
2023-01-26 14:42:49 -0300

    add create LandingPage

---
+++ components/LandingPage.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const LandingPage = (): JSX.Element => {
+  return <LandingPageContainer></LandingPageContainer>;
+};
+
+const LandingPageContainer = styled.div``;
+
+export default LandingPage;
