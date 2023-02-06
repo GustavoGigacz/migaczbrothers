b677a887201afa0df703e19ff2aeb5f27a966f0c
GustavoGigacz <gustavomigacz@gmail.com>
2023-02-06 15:42:40 -0300

    add finish Footer

--- components/ Footer.tsx
+++ components/ Footer.tsx
@@ -1,5 +1,79 @@
+import styled from "styled-components";
+
+import LinkedinIcon from "./icons/LinkedinIcon";
+import MailIcon from "./icons/MailIcon";
+
 const Footer = (): JSX.Element => {
-  return <></>;
+  return (
+    <>
+      <FooterSection>
+        <FooterContent>
+          <FooterContainer>
+            <LanguageContent>
+              <Language>EN</Language>
+              <Language>PT</Language>
+            </LanguageContent>
+            <IconContent>
+              <LinkedinIcon />
+              <MailIcon />
+            </IconContent>
+            <NavContent>
+              <NavLink>Home</NavLink>
+              <NavLink>About Us</NavLink>
+              <NavLink>Contact Us</NavLink>
+            </NavContent>
+          </FooterContainer>
+          <CopyrightContainer>
+            Copyright © 2023 Migacz Brothers
+          </CopyrightContainer>
+        </FooterContent>
+      </FooterSection>
+    </>
+  );
 };
+
+const FooterSection = styled.div`
+  background-color: var(--dark-secondary);
+  max-width: 100%;
+`;
+const FooterContent = styled.div`
+  width: 1400px;
+  margin: 0 auto;
+`;
+const FooterContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  padding: 44px 16px;
+`;
+const LanguageContent = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-self: flex-start;
+  gap: 30px;
+`;
+const Language = styled.li`
+  color: var(--white-color-font);
+  text-decoration: underline;
+`;
+const IconContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-self: center;
+  gap: 30px;
+  color: var(--white-color-font);
+`;
+const NavContent = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-self: flex-end;
+  gap: 30px;
+`;
+const NavLink = styled.ul`
+  color: var(--white-color-font);
+`;
+const CopyrightContainer = styled.div`
+  text-align: center;
+  color: var(--secondary-color-font);
+`;
 
 export default Footer;
---
+++ components/icons/MailIcon.tsx
@@ -0,0 +1,24 @@
+const MailIcon = (): JSX.Element => (
+  <svg
+    width='16'
+    height='14'
+    viewBox='0 0 16 14'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M15 2.5L8 7.75L1 2.5'
+      stroke='#ffffff'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+    ></path>
+    <path
+      d='M2.4 1H13.6C14.37 1 15 1.675 15 2.5V11.5C15 12.325 14.37 13 13.6 13H2.4C1.63 13 1 12.325 1 11.5V2.5C1 1.675 1.63 1 2.4 1Z'
+      stroke='#ffffff'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+    ></path>
+  </svg>
+);
+
+export default MailIcon;
