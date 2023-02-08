51264be844628356ed1c77fdf908ce453d9e8233
GustavoGigacz <gustavomigacz@gmail.com>
2023-02-08 20:53:25 -0300

    add 404

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,15 @@ import type { AppProps } from "next/app";
 import NavBar from "../components/NavBar";
 import Footer from "../components/ Footer";
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <GlobalStyle />
-      <Variables />
-      <NavBar />
-      <Component {...pageProps} />
-      <Footer />
-    </>
-  );
-};
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <GlobalStyle />
+    <Variables />
+    {/* <NavBar /> */}
+    <MobileNavbar />
+    <Component {...pageProps} />
+    <Footer />
+  </>
+);
 
 export default App;
